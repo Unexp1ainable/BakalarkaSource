8d32d8b3ff8db227f8628a7023817fcd35f6cb20
Unexp1ainable <samorepka314@gmail.com>
2022-03-12 13:42:33 +0100

    ImageLabel

--- app/widgets/ImageLoader.py
+++ app/widgets/ImageLoader.py
@@ -5,11 +5,14 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import QAction, QPaintEvent, QPixmap, QTransform, QImage
 from PySide6.QtCore import Slot, Qt
 from PySide6 import QtCore
+from widgets.ImageLabel import ImageLabel
 
 
 class ImageLoader(QWidget):
     def __init__(self):
         super().__init__()
         layout = QGridLayout()
+
+        self.img
 
         self.setLayout(layout)
